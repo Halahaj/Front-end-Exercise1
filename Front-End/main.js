325ffc7255c3e8b417761f8fa3c03d55095cc06b
Halahaj <123293197+Halahaj@users.noreply.github.com>
2023-01-22 12:21:27 +0300

    Add files via upload

---
+++ Front-End/main.js
@@ -0,0 +1,11 @@
+const name= document.getElementById("Name");
+const phone= document.getElementById("Email");
+const email= document.getElementById("PhoneNumber");
+const find= document.getElementById("select");
+
+function handlesubmit (){
+console.log("name"+name.value);
+console.log("Email"+Email.value);
+console.log(PhoneNumber.value);
+console.log(select.value);}
+
